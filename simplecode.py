15fa9d264fc947a116b464bdb7948e74833f7b0c
Apeksha Math <apeksha@infotracktelematics.com>
2023-12-20 10:06:37 +0530

    Simpleprograms

---
+++ simplecode.py
@@ -0,0 +1,11 @@
+print("hello world")
+
+my_list = [2,3,5,4,6]
+my_list.sort()
+print(my_list)
+
+# mixed list
+prime_numbers = [2, 3, 5]
+# copying a list
+numbers = prime_numbers.copy()
+print('Copied List:', numbers)
